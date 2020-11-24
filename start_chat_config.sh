77601c98170a4980e1324524a374b38f8ff104e5
Juan Sebastian <juan.montealegre@smartquick.com.co>
2020-11-23 19:05:49 -0500

    Show the forms messages errors

--- start_chat_config.sh
+++ start_chat_config.sh
@@ -2,14 +2,14 @@
 
 # Script to inizializated project and run all migrations and 
 # install requirements
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $DIR
 echo "Install enviroment..."
 sudo apt-get -y install python3-pip
 sudo pip3 install virtualenv
 virtualenv -p python3 .env
 echo "Activate enviroment"
 source .env/bin/activate
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
 
 echo "Install Requiremets"
 pip install -r requirements.txt
@@ -20,7 +20,7 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 
 echo "Run Docker redis"
-docker run -p 6379:6379 -d redis:5
+sudo docker run -p 6379:6379 -d redis:5
 
 
 echo "Run project"
--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-
 class NewUserForm(UserCreationForm):
     """Form to new users."""
 
