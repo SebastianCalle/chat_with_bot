eec03bd26d94e57efd85e1ebe4ed9a454c4a4b00
Juan Sebastian <juan.montealegre@smartquick.com.co>
2020-12-08 21:32:10 -0500

    Add new models to contact and room for the messages

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Models.
-from chat.models import Message
+from chat.models import Message, Room, Contact
 
 
+admin.site.register(Contact)
 admin.site.register(Message)
+admin.site.register(Room)
--- chat/models.py
+++ chat/models.py
@@ -10,16 +10,50 @@ from model_utils.models import TimeStampedModel, SoftDeletableModel
 User = get_user_model()
 
 
+class Contact(models.Model):
+    """Contact model."""
+    user = models.ForeignKey(
+        User,
+        related_name='frieds',
+        on_delete=models.CASCADE
+    )
+    friends = models.ManyToManyField('self', blank=True)
+
+    def __str__(self):
+        """String representation."""
+        return f"{self.user.username}"
+
+
 class Message(TimeStampedModel, SoftDeletableModel, models.Model, ):
     """Messages Model."""
-    author = models.ForeignKey(
-        User,
-        related_name='author',
+    contact = models.ForeignKey(
+        Contact,
+        related_name='messages',
         on_delete=models.CASCADE
     )
     content = models.TextField()
 
     def __str__(self):
         """String representation."""
-        return f'{self.author}'
+        return f'{self.contact.user.username}'
 
+
+class Room(TimeStampedModel, SoftDeletableModel, models.Model, ):
+    """Room Model."""
+    messages = models.ManyToManyField(
+        Message,
+        blank=True
+    )
+    participants = models.ManyToManyField(
+        Contact,
+        related_name='rooms',
+    )
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        """String representation."""
+        return f"{self.name}"
+
+    def get_last_fifteen_messages(self):
+        """Return the las fifteen messages."""
+        return self.messages.order_by('created')[:50]
